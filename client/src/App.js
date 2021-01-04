9cacda600eb81f5caf71d5f6df2bdadd487d54d8
Toluwalase <toluakintoye@gmail.com>
2021-01-04 21:39:19 +0100

    Rearranged routes to fix course detail rendering in place of the course update and create course is rendering the not found page.

--- client/src/App.js
+++ client/src/App.js
@@ -39,12 +39,12 @@ function App() {
 
           <Switch>
               <Route exact path="/"  component={Courses} />
+              <PrivateRoute path="/courses/create" component={CreateCourseWithContext} />
+              <PrivateRoute path="/courses/:id/update" component={UpdateCourseWithContext} />
+              <Route path="/courses/:id" component={CourseDetailWithContext} />
               <Route path="/signin" component={UserSignInWithContext} />
               <Route path="/signup" component={UserSignUpWithContext} />
               <Route path="/signout" component={UserSignOutWithContext} />
-              <Route path="/courses/:id" component={CourseDetailWithContext} />
-              <PrivateRoute path="/courses/create" component={CreateCourseWithContext} />
-              <PrivateRoute path="/courses/:id/update" component={UpdateCourseWithContext} />
               <Route path="/forbidden" component={Forbidden} />
               <Route path="/error" component={UnhandledError} />
               <Route path="/notfound" component={NotFound} />
