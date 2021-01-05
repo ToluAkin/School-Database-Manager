8c3f0db26a1ce086ce73e1d2cc8c46e2f83375d3
Toluwalase <toluakintoye@gmail.com>
2021-01-05 22:59:27 +0100

    updated rendering Not found component

--- client/src/App.js
+++ client/src/App.js
@@ -47,7 +47,7 @@ function App() {
               <Route path="/signout" component={UserSignOutWithContext} />
               <Route path="/forbidden" component={Forbidden} />
               <Route path="/error" component={UnhandledError} />
-              <Route path="/notfound" component={NotFound} />
+              <Route component={NotFound} />
           </Switch>
         </div>
       </Router>
